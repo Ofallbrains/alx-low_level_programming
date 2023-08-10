cc30992605f96146179a32400b900b2aa29afe26
Ofallbrains <ofallbrains@gmail.com>
2023-08-10 02:56:06 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * 
- * Return: 0 (success)
+ * main - A program that print a line with puts function
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	retun(0);
-}		
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
