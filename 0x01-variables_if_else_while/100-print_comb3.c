920849307775173fd22270badea1fab4a8642f41
Ofallbrains <ofallbrains@gmail.com>
2023-08-11 06:39:38 -0700

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,20 +5,33 @@
  */
 int main(void)
 {
-int a, b;
-for (a = '0'; a < '9'; a++)
-{
-for (b = a + 1; b <= '9'; b++)
-{
-if (b != a)
-{
-if (a == '8' && b == '9')
-continue;
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	int a, b = '0', c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (c = b; c <= '9'; c++)
+		{
+			if (a != c)
+			{
+				putchar(a);
+				putchar(c);
+			}
+			if (a == c)
+			{
+				continue;
+			}
+			if (a == '8' && c == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		b++;
+	}
+	putchar('\n');
+	return (0);
 }
