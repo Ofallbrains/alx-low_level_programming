b1dec17ca5b2b6957123d04c003ab757cb800b2f
Ofallbrains <ofallbrains@gmail.com>
2023-08-11 03:48:04 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabet
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char c;
+
+c = 'a';
+while
+	(c <= 'z') {
+		putchar(c);
+		c++;
+	}
+putchar('\n');
+return (0);
+}
