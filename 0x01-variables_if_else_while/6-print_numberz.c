c56b06c685dc8a28191e2c5a1d6a527c6c4f0adb
Ofallbrains <ofallbrains@gmail.com>
2023-08-11 04:37:14 -0700

    numbersz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints single digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int c;
+c = 0;
+while
+	(c < 10) {
+		putchar(c + '0');
+		c++;
+	}
+putchar('\n');
+return (0);
+}
