8429180358e4c2675f7a717a9eaf24d83ad331b5
Ofallbrains <ofallbrains@gmail.com>
2023-08-11 04:18:03 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints all the letters except q and e
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while
+	(c <= 'z') {
+		if ((c != 'q' && c != 'e') && c <= 'z')
+			putchar(c);
+		c++;
+	}
+putchar('\n');
+return (0);
+}
