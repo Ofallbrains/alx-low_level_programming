d770797fa024c4945de48e7f7d45af746f6c7ad7
Ofallbrains <ofallbrains@gmail.com>
2023-08-11 05:06:56 -0700

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of single digits
+ * Return: 0 (success
+ */
+int main(void)
+{
+int a;
+for  (a = 0; a < 10; a++)
+{
+	putchar(a + '0');
+	if (a < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
