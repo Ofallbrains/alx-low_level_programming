5f3fa47b46bc7152ed68ed95c571beb8819ffc41
Ofallbrains <ofallbrains@gmail.com>
2023-08-14 14:56:13 -0700

    timetable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,28 +5,35 @@
  */
 void times_table(void)
 {
-int a, b, c;
-for (a = 0; a <= 9; a++)
-{
-_putchar('0');
-_putchar(',');
-_putchar(' ');
-for (b = 1; b <= 9; b++)
-{
-c = (a * b);
-if ((c / 10) > 0)
-{
-_putchar((c / 10) + '0');
-}
-else
-{
-_putchar(' ');
-}
-_putchar((c % 10) + '0');
-if (b < 9)
-_putchar(',');
-_putchar(' ');
-}
-_putchar('\n');
-}
+	int i, j, n;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			n = i * j;
+
+			if ((n / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(n + '0');
+
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
