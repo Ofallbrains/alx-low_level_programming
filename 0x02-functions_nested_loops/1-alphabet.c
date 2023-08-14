e6deb95e8d5d05221bd6ebf20630be3a723d1f92
Ofallbrains <ofallbrains@gmail.com>
2023-08-14 02:11:00 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints alphabet in lowercase
+ *
+ */
+void print_alphabet(void)
+{
+char al;
+for (al = 'a'; al <= 'z'; al++)
+{
+	_putchar(al);
+}
+_putchar('\n');
+}
