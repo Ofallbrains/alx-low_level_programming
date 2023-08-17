25873a2267251d58ff66599df4dc974146904d2f
Ofallbrains <ofallbrains@gmail.com>
2023-08-17 03:41:57 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers 0 - 9
+ * Return: no return
+ */
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
