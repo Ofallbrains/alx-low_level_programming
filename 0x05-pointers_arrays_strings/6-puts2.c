971ec7e638854c4e269ae4b29f4770a15ffda6cd
Ofallbrains <ofallbrains@gmail.com>
2023-08-21 03:28:06 -0700

    put2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: character of a string
+ * Return: 0
+ */
+void puts2(char *str)
+{
+int c;
+for (c = 0; str[c] != '\0'; c++)
+{
+if (c % 2 == 0)
+{
+_putchar (str[c]);
+}
+}
+_putchar('\n');
+}
