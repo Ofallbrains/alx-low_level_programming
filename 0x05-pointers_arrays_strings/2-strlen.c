24c93bcdcf7f15ca91c31ec297b6b1342d3cc5ec
Ofallbrains <ofallbrains@gmail.com>
2023-08-21 01:49:16 -0700

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of the string
+ * @s: string character
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+int len;
+for (len = 0; s[len] != '\0'; len++)
+{
+/* empty */
+}
+return (len);
+}
