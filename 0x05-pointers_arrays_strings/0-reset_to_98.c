ef6292233f5733fcf1a4630b6a3f4960a3918f8c
Ofallbrains <ofallbrains@gmail.com>
2023-08-21 00:20:03 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - functions takes a pointer to an int
+ * and updates the value to 98
+ * @n: value of a deferencing pointer
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
