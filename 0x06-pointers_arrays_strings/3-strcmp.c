5d4f9f5c17f1dfebbb518912f976587f35fc9088
Ofallbrains <ofallbrains@gmail.com>
2023-08-23 02:54:44 -0700

    compare string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+int j = 0;
+for (i = 0; j == 0 && s1[i] != '\0'; i++)
+{
+j = s1[i] - s2[i];
+}
+return (j);
+}
