926c0dfd3471230bf9a7597c40f8351fa5a6c8f1
Ofallbrains <ofallbrains@gmail.com>
2023-08-23 04:18:50 -0700

    string cat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,15 @@
+#include <string.h>
 #include "main.h"
 /**
- * _strncat -  concatenates two strings
+ * _strncat - concatenates two strings
  * @dest: destination string
  * @src: source string
- * @n: no of element from src
- * Return: destination + no of src
+ * @n: no of bytes from src
+ * Return: pointer to source string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int len, i;
-for (len = 0; dest[len] != '\0'; len++)
-{
+strncat(dest, src, n);
 
-}
-for (i = 0; i < n && src[i] != 0; i++)
-{
-dest[len + i] = src[i];
-}
 return (dest);
 }
