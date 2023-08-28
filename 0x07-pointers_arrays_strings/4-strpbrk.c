c7a3c5b45a2ed8bfb2f69dc1cc79908e30faaab5
Ofallbrains <ofallbrains@gmail.com>
2023-08-28 03:21:11 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: input string
+ * @accept: input value
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int j;
+while (*s)
+{
+for (j = 0; accept[j]; j++)
+{
+if (*s == accept[j])
+return (s);
+}
+s++;
+}
+return ('\0');
+}
