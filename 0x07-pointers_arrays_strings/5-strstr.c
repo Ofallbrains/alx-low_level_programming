5834d46417a9d9f8bad654a6abe935bf11e33849
Ofallbrains <ofallbrains@gmail.com>
2023-08-28 03:32:22 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr -  locates a substring
+ * @haystack: input string
+ * @needle: input substring
+ * Return: NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{	
+char *b = haystack;
+char *c = needle;
+while (*b == *c && *c != '\0')
+{
+b++;
+c++;
+}
+
+if (*c == '\0')
+return (haystack);
+}
+return (0);
+}
