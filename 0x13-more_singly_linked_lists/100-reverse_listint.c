52e6e642ef470947d03cf8c30ef6f9c5487dbbe2
Ofallbrains <ofallbrains@gmail.com>
2023-09-25 16:17:05 -0700

    certified coder

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * reverse_listint - a function that reverses a linked list
+ * @head: linked list
+ * Return: a function that reverses a list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *p;
+	listint_t *n;
+
+	p = NULL;
+	n = NULL;
+
+	while (*head != NULL)
+	{
+		n = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
+	}
+
+	*head = p;
+	return (*head);
+}
